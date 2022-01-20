29298a08f344d0aadd316ad6a8635b2a94d1aede
DGCP3 <zdagmawig1@gmail.com>
2022-01-21 02:42:35 +0300

    final

--- designs.js
+++ designs.js
@@ -2,32 +2,24 @@ const colorPicker = document.getElementById("colorPicker");
 const inputHeight = document.getElementById("inputHeight");
 const inputWidth = document.getElementById("inputWidth");
 const table = document.getElementById("pixelCanvas");
+const submit = document.getElementById("submit");
+submit.addEventListener("click", makeGrid);
 
 function makeGrid(e) {
   e.preventDefault();
-  table.innerHTML = ""; 
-  for (let column = 1; column <= inputHeight.value; column++) {
-    const newRow = createElement("tr", "", column);
-    table.appendChild(newRow);
-    for (let row = 1; row <= inputWidth.value; row++) {
-      const cell = createElement("td", row, "");
+  table.innerHTML = "";
+  for (let column = 0; column < inputHeight.value; column++) {
+    const newRow = table.insertRow();
+    for (let row = 0; row < inputWidth.value; row++) {
+      const cell = newRow.insertCell();
       cell.style.background = "white";
       cell.addEventListener("click", colorChanger);
-      document.getElementsByClassName(column)[0].appendChild(cell);
     }
   }
 }
-const button = document.getElementById("submit");
-button.addEventListener("click", makeGrid);
+
 function colorChanger(e) {
   e.target.style.background === "white"
     ? (e.target.style.background = colorPicker.value)
     : (e.target.style.background = "white");
-}
-
-function createElement(elementType, elementId, elementClass) {
-  const element = document.createElement(elementType);
-  if (elementId) element.id = elementId;
-  if (elementClass) element.className = elementClass;
-  return element;
 }
